faba9380887d6af8a8c9fa39e484e40707a32528
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-20 16:00:17 +0530

    test

---
+++ constructor1.js
@@ -0,0 +1,12 @@
+class test{
+    constructor(){
+        console.log("constructor special method");
+    }
+
+    getdetails(){
+        console.log("normal method");
+    }
+
+}
+new test(10,20);
+new test();
