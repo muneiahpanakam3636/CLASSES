454f22d254dcd07f02ef3b9b1664bf266a6bcb04
muneiahpanakam3636 <muneiahpanakam3636@gmail.com>
2024-06-20 16:22:00 +0530

    test

---
+++ constructor2.js
@@ -0,0 +1,12 @@
+class test{
+    constructor(a,b,c){
+        console.log(a,b,c);
+    }
+
+    getDetails(){
+        console.log("normal method");
+
+    }
+}
+new test(10,20,30);
+new test(1,2,3)
